d0d14ced752eb836aadde1308e6e751eb0759562
Wicaksono <tri.wicaksono69@gmail.com>
2025-06-24 09:45:21 +0700

    hotfix: profile endpoint

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -31,11 +31,43 @@ class UserRepository implements UserInterface
      *
      * @return \App\Models\User
      */
-    public function profile(int $id): User
+    public function profile(int $id): array
     {
-        $user = User::select('id', 'nik', 'email', 'name')
+        $user = User::select('id', 'nik', 'email')
             ->where('id', $id)
             ->first();
-        return $user;
+
+        if (!$user) {
+            return array();
+        }
+
+        $kependudukan = DB::connection('mysql-provider')
+            ->table('s_kependudukan_anggota')
+            ->join('s_kependudukan', 's_kependudukan_anggota.nomor_kk', '=', 's_kependudukan.nomor_kk')
+            ->select(
+                "s_kependudukan_anggota.nama",
+                "s_kependudukan_anggota.nomor_kk",
+                "s_kependudukan.alamat",
+                "s_kependudukan_anggota.tempat_lahir",
+                "s_kependudukan_anggota.tanggal_lahir",
+                "s_kependudukan_anggota.nama_ayah",
+                "s_kependudukan_anggota.nama_ibu"
+            )
+            ->where('s_kependudukan_anggota.nik', $user->nik)
+            ->first();
+
+        $data = [
+            'id' => $user->id,
+            'nik' => $user->nik,
+            'email' => $user->email,
+            'nama' => $kependudukan ? $kependudukan->nama : null,
+            'alamat' => $kependudukan ? $kependudukan->alamat : null,
+            'tempat_lahir' => $kependudukan ? $kependudukan->tempat_lahir : null,
+            'tanggal_lahir' => $kependudukan ? $kependudukan->tanggal_lahir : null,
+            'nama_ayah' => $kependudukan ? $kependudukan->nama_ayah : null,
+            'nama_ibu' => $kependudukan ? $kependudukan->nama_ibu : null,
+        ];
+
+        return $data;
     }
 }
